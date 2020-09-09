a91e9701b52552129558501cae792c8f4d6a1679
cyasmani <yasmanicastaneda@yahoo.com>
2020-09-08 19:20:54 -0700

    MVP

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,44 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getByPlaceholderText, fireEvent, getByText } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
+import { Simulate } from "react-dom/test-utils";
 
-test("renders App without crashing", () => {
-  render(<App />);
-});
+// test("renders App without crashing", () => {
+//   render(<App />);
+// })
+
+
+test("Tests contact form", async () => {
+
+  const { getByPlaceholderText, getByLabelText , getByText} = render(<ContactForm />);
+  const firstName = getByPlaceholderText("Edd");
+  const lastName = getByPlaceholderText("Burke");
+  const email = getByText("Email*");
+  const message = getByText("Message");
+  
+    
+  fireEvent.change(firstName, {
+    target: {value: "Yasmani"},
+
+  })
+
+  fireEvent.change(lastName, {
+    target: {value: "Castaneda"},
+
+  })
+
+  fireEvent.change(email, {
+    target: "yasmanicastaneda@yahoo.com"},
+    //Why cant I use value in this field?
+
+  )
+
+  fireEvent.change(message, {
+    target: "message"
+  })
+})
+
+  
+  
+
